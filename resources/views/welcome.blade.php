82df72adb3146b39842ac0da7e756a96844ea50b
Robertto Flores <roberttoflores@iMac-Pro-de-Robertto.local>
2022-01-14 16:42:27 -0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body >
+        <div class="container h-full	 mx-auto px-4 bg-gradient-to-r from-purple-500 to-pink-500 flex flex-wrap justify-center">
+            <div class="w-6/12 sm:w-4/12 px-4">
+                <img src="https://pbs.twimg.com/profile_images/1454874783980609537/lrp2EAEW_400x400.jpg" class="shadow-lg rounded-full max-w-full h-auto align-middle border-none">
+            </div>
+            <div class="container mx-auto flex justify-center" >
+                <h3 class="font-bold text-lg	text-stone-900	">Robertto Flores</h3>
+
+            </div>
+            <div class="container mx-auto flex justify-center" >
+                <h3 class="">soporte tecnico lol </h3>
+
+            </div>
+        </div>
+
+
+
+
+    </body>
+</html>
