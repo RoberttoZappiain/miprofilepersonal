6c01dd13eef0fba15a07bbb26f4f21da52b69e01
Robertto Flores <roberttoflores@iMac-Pro-de-Robertto.local>
2022-01-15 19:13:20 -0700

    ¨gisst¨

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,26 +13,39 @@
         <!-- Styles -->
 
 
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
+
     </head>
     <body >
         <div class="container h-full	 mx-auto px-4 bg-gradient-to-r from-purple-500 to-pink-500 flex flex-wrap justify-center">
-            <div class="w-6/12 sm:w-4/12 px-4">
+            <div class="w-6/12 mt-10 sm:w-4/12 px-4">
                 <img src="https://pbs.twimg.com/profile_images/1454874783980609537/lrp2EAEW_400x400.jpg" class="shadow-lg rounded-full max-w-full h-auto align-middle border-none">
+                <div class="xl:text-5xl flex font-extrabold justify-center">
+                    <span class="bg-clip-text text-red bg-gradient-to-r from-pink-800 to-violet-700">
+                      Hello world
+                    </span>
+                  </div>
             </div>
-            <div class="container mx-auto flex justify-center" >
-                <h3 class="font-bold text-lg	text-stone-900	">Robertto Flores</h3>
+
+            <div class="container mx-auto  justify-center" >
 
             </div>
             <div class="container mx-auto flex justify-center" >
-                <h3 class="">soporte tecnico lol </h3>
+                <h3 class="">soporte tecnico de lol </h3>
 
             </div>
+            <div class="text-5xl font-extrabold flex-col justify-center">
+                <div>01</div>
+  <div>02</div>
+  <div>03</div>
+                <span class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+                    Ingeniero en Gestion y Desarrollo de Software orientado al desarrollo multiplataforma.
+                </span>
+                <button class="rounded-full border-2 border-rose-500	">Contactame</button>
+              </div>
+
+
         </div>
+
 
 
 
